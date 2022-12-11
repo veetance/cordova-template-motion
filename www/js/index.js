53b5defd229269f2c27063e5647303775a09be84
veetance <divineokos@hotmail.com>
2022-12-10 18:38:30 -0700

    gyro working and top nav done - next add meter

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,30 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
+
+// make .leadBoard  and logo width the same as its height/ my fix for the aspect ratio issue//
+function refreshaspectRatio() {
+
+var leadBoard = $(".leadBoard");
+var leadBoardH = leadBoard.height();
+leadBoard.css("width", leadBoardH);
+
+var logo = $(".logo");
+var logoH = logo.height();
+logo.css("width", logoH);
+
+}
+
+// call refreshaspectRatio function each time the top-nav width changes
+refreshaspectRatio();
+$(window).resize(function () {  
+    refreshaspectRatio();
+});
+
+
+
+
+
+
 function onDeviceReady() {
   // Cordova is now initialized. Have fun!
 
@@ -36,20 +61,25 @@ function onDeviceReady() {
     box.css("left", currentX + y + movebox);
     box.css("top", currentY + x + movebox);
 
+    // make .platform act as a portal.
+    var pWW = $(".platform").width();
+    var pHH = $(".platform").height();
 
+    if (currentX > pWW) {
+      box.css("left", 0);
+    } else if (currentX < -20) {
+      box.css("left", pWW);
+    } else if (currentY > pHH) {
+      box.css("top", 0);
+    } else if (currentY < 0) {
+      box.css("top", pHH);
+    }
 
-    Number.prototype.map = function (in_min, in_max, out_min, out_max) {
-      return (
-        ((this - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-      );
-    };
+    
+    //////////////////////////////////////
 
-    var r = z.map(0, 360, 0, 255);
-    var g = x.map(-180, 180, 0, 255);
-    var b = y.map(-90, 90, 0, 255);
-
-    $("body").css("background", "rgb(" + r + "," + g + "," + b + ")");
   }
+  
 }
 
 // reset page function rloads the page on the touch or press of .lives
@@ -57,6 +87,8 @@ function onDeviceReady() {
 $(".lives").on("touchend click", function () {
   location.reload();
 });
+
+
 
 
 
